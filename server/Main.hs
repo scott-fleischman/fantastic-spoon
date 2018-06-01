b69d71e0645231f8ae720a36ec68bb5d1a72df49
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-05-31 21:19:32 -0700

    Use plotly; add populate random users.

--- server/Main.hs
+++ server/Main.hs
@@ -14,7 +14,7 @@
 
 import           Data.Aeson ((.=))
 import qualified Data.Aeson as Aeson
-import qualified Control.Monad.IO.Class as Monad.IO.Class
+import qualified Control.Monad.IO.Class as Monad.IO
 import qualified Control.Monad.Logger as Monad.Logger
 import qualified Control.Monad.Trans.Resource as Monad.Trans.Resource
 import qualified Data.String.Interpolate as String.Interpolate
@@ -28,6 +28,7 @@ import qualified Database.Persist.TH as Persist.TH
 import           GHC.Generics (Generic)
 import qualified Network.HTTP.Types as HTTP.Types
 import qualified System.Environment as Environment
+import qualified System.Random as Random
 import qualified Text.Read
 import qualified Yesod
 import           Yesod.Static (Static)
@@ -56,6 +57,7 @@ data FantasticSpoon = FantasticSpoon
 Yesod.mkYesod "FantasticSpoon" [Yesod.parseRoutes|
 / HomeR GET
 /user CreateUserR POST
+/populate PopulateUsersR POST
 /static StaticR Static appStatic
 |]
 
@@ -92,117 +94,38 @@ getHomeR = do
     _maxAgeJS = Text.pack . show $ maximum userAges
   Yesod.sendResponseStatus HTTP.Types.status200 $ RawHtml [String.Interpolate.i|<!DOCTYPE html>
 <meta charset="utf-8">
-<style>
-
-.bars rect {
-  fill: steelblue;
-}
-
-.axis text {
-  font: 10px sans-serif;
-}
-
-.axis path, .axis line {
-  fill: none;
-  stroke: #000;
-  shape-rendering: crispEdges;
-}
-
-</style>
+<head>
+<script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+</head>
 <body>
-<script src="https://d3js.org/d3.v3.min.js"></script>
+<div id="myDiv"></div>
 <script>
-
 var userAges = #{userAgesJS};
-d3.select("body")
-  .datum(userAges)
-  .call(histogramChart());
-
-function histogramChart() {
-  var margin = {top: 0, right: 0, bottom: 20, left: 0},
-      width = 960,
-      height = 500;
-
-  var histogram = d3.layout.histogram(),
-      x = d3.scale.ordinal(),
-      y = d3.scale.linear(),
-      xAxis = d3.svg.axis().scale(x).orient("bottom").tickSize(6, 0);
-
-  function chart(selection) {
-    selection.each(function(data) {
-
-      // Compute the histogram.
-      data = histogram(data);
-
-      // Update the x-scale.
-      x   .domain(data.map(function(d) { return d.x; }))
-          .rangeRoundBands([0, width - margin.left - margin.right], .1);
-
-      // Update the y-scale.
-      y   .domain([0, d3.max(data, function(d) { return d.y; })])
-          .range([height - margin.top - margin.bottom, 0]);
-
-      // Select the svg element, if it exists.
-      var svg = d3.select(this).selectAll("svg").data([data]);
-
-      // Otherwise, create the skeletal chart.
-      var gEnter = svg.enter().append("svg").append("g");
-      gEnter.append("g").attr("class", "bars");
-      gEnter.append("g").attr("class", "x axis");
-
-      // Update the outer dimensions.
-      svg .attr("width", width)
-          .attr("height", height);
-
-      // Update the inner dimensions.
-      var g = svg.select("g")
-          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-      // Update the bars.
-      var bar = svg.select(".bars").selectAll(".bar").data(data);
-      bar.enter().append("rect");
-      bar.exit().remove();
-      bar .attr("width", x.rangeBand())
-          .attr("x", function(d) { return x(d.x); })
-          .attr("y", function(d) { return y(d.y); })
-          .attr("height", function(d) { return y.range()[0] - y(d.y); })
-          .order();
-
-      // Update the x-axis.
-      g.select(".x.axis")
-          .attr("transform", "translate(0," + y.range()[0] + ")")
-          .call(xAxis);
-    });
-  }
-
-  chart.margin = function(_) {
-    if (!arguments.length) return margin;
-    margin = _;
-    return chart;
-  };
-
-  chart.width = function(_) {
-    if (!arguments.length) return width;
-    width = _;
-    return chart;
-  };
-
-  chart.height = function(_) {
-    if (!arguments.length) return height;
-    height = _;
-    return chart;
-  };
-
-  // Expose the histogram's value, range and bins method.
-  d3.rebind(chart, histogram, "value", "range", "bins");
-
-  // Expose the x-axis' tickFormat method.
-  d3.rebind(chart, xAxis, "tickFormat");
-
-  return chart;
-}
-
+var trace = {
+  x: userAges,
+  type: 'histogram',
+  marker: {
+     color: "rgba(220, 226, 189, 1)",
+  },
+  name: "All Users",
+  xbins: {
+    end: 100,
+    size: 5, 
+    start: 10
+  },
+  autobinx: false,
+  autobiny: true,
+};
+var layout = {
+  bargap: 0.02,
+  title: "Ages of Users", 
+  xaxis: {title: "Age"},
+  yaxis: {title: "Count"}
+};
+var data = [trace];
+Plotly.newPlot('myDiv', data, layout);
 </script>
+</body>
   |]
 
 postCreateUserR :: Yesod.HandlerFor FantasticSpoon Aeson.Value
@@ -217,6 +140,23 @@ postCreateUserR = do
   userId <- Yesod.runDB $ Persist.insert user
   return $ Aeson.object ["userId" .= Aeson.toJSON userId]
 
+nameList :: [Text]
+nameList = ["olivia","ruby","emily","grace","jessica","chloe","sophie","lily","amelia","evie","mia","ella","charlotte","lucy","megan","ellie","isabelle","isabella","hannah","katie","ava","holly","summer","millie","daisy","phoebe","freya","abigail","poppy","erin","emma","molly","imogen","amy","jasmine","isla","scarlett","leah","sophia","elizabeth","eva","brooke","matilda","caitlin","keira","alice","lola","lilly","amber","isabel","lauren","georgia","gracie","eleanor","bethany","madison","amelie","isobel","paige","lacey","sienna","libby","maisie","anna","rebecca","rosie","tia","layla","maya","niamh","zara","sarah","lexi","maddison","alisha","sofia","skye","nicole","lexie","faith","martha","harriet","zoe","eve","julia","aimee","hollie","lydia","evelyn","alexandra","maria","francesca","tilly","florence","alicia","abbie","emilia","courtney","maryam","esme","jack","oliver","thomas","harry","joshua","alfie","charlie","daniel","james","william","samuel","george","joseph","lewis","ethan","mohammed","dylan","benjamin","alexander","jacob","ryan","liam","jake","max","luke","tyler","callum","matthew","jayden","oscar","archie","adam","riley","harvey","harrison","lucas","muhammad","henry","isaac","leo","connor","edward","finley","logan","noah","cameron","alex","owen","rhys","nathan","jamie","michael","mason","toby","aaron","charles","ben","theo","louis","freddie","finlay","leon","harley","david","mohammad","reece","kian","kai","kyle","brandon","hayden","zachary","kieran","luca","ashton","bailey","sebastian","gabriel","sam","evan","bradley","elliot","john","taylor","joe","corey","reuben","joel","robert","ellis","blake","aidan","louie","christopher","ewan","jay","morgan","billy","sean","zak"]
+
+postPopulateUsersR :: Yesod.HandlerFor FantasticSpoon Aeson.Value
+postPopulateUsersR = do
+  stdGen <- Monad.IO.liftIO Random.getStdGen
+  let
+    ageRange = (10, 100)
+    fakePassword = "pwd"
+    ages = take (length nameList) (Random.randomRs ageRange stdGen)
+    nameAgePairs = zip nameList ages
+    users = fmap (\(name, age) -> User name fakePassword age) nameAgePairs
+  Yesod.runDB $ do
+    _ <- mapM Persist.insert users
+    return ()
+  return $ Aeson.object ["usersAdded" .= Aeson.toJSON (length users)]
+
 main :: IO ()
 main = do
   connectionString    <- fmap (Text.Encoding.encodeUtf8 . Text.pack)  $ Environment.getEnv "DATABASE_URL"
@@ -226,7 +166,7 @@ main = do
     $(Monad.Logger.logInfo) $ "Open connection count: " <> (Text.pack . show) openConnectionCount
     $(Monad.Logger.logInfo) $ "Port: " <> (Text.pack . show) port
 
-    Persist.Postgresql.withPostgresqlPool connectionString openConnectionCount $ \pool -> Monad.IO.Class.liftIO $ do
+    Persist.Postgresql.withPostgresqlPool connectionString openConnectionCount $ \pool -> Monad.IO.liftIO $ do
       let
         runMigrationAction = Persist.Postgresql.runMigration migrateAll
         runMigrationWithPool = Persist.Postgresql.runSqlPool runMigrationAction pool
