dd23f7a268273af49545df57925b0ed0b2940ba3
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2018-05-31 17:04:02 -0700

    Use chart from d3.
    Source:
    https://bl.ocks.org/mbostock/1933560

--- server/Main.hs
+++ server/Main.hs
@@ -17,6 +17,7 @@ import qualified Data.Aeson as Aeson
 import qualified Control.Monad.IO.Class as Monad.IO.Class
 import qualified Control.Monad.Logger as Monad.Logger
 import qualified Control.Monad.Trans.Resource as Monad.Trans.Resource
+import qualified Data.String.Interpolate as String.Interpolate
 import           Data.Semigroup ((<>))
 import           Data.Text (Text)
 import qualified Data.Text as Text
@@ -68,17 +69,141 @@ instance Yesod.YesodPersist FantasticSpoon where
     let pool = appConnectionPool app
     Persist.Postgresql.runSqlPool action pool
 
--- List all people in the database
+newtype RawHtml = RawHtml String
+instance Yesod.ToContent RawHtml where
+  toContent (RawHtml htmlText) = Yesod.toContent htmlText
+instance Yesod.ToTypedContent RawHtml where
+  toTypedContent rawHtml = Yesod.TypedContent Yesod.typeHtml (Yesod.toContent rawHtml)
+instance Yesod.HasContentType RawHtml where
+  getContentType _ = Yesod.typeHtml
+
 getHomeR :: Yesod.HandlerFor FantasticSpoon Yesod.Html
 getHomeR = do
   users <- Yesod.runDB $ Persist.selectList [] [Persist.Asc UserAge]
-  let userAges = fmap (userAge . Persist.entityVal) users
-  Yesod.defaultLayout
-    [Yesod.whamlet|
-      <ul>
-        $forall age <- userAges
-          <li>#{age}
-    |]
+  let
+    userAges = fmap (userAge . Persist.entityVal) users
+    userAgesJS =
+      Text.concat
+        [ "["
+        , Text.intercalate "," $ fmap (Text.pack . show) userAges
+        , "]"
+        ]
+    _minAgeJS = Text.pack . show $ minimum userAges
+    _maxAgeJS = Text.pack . show $ maximum userAges
+  Yesod.sendResponseStatus HTTP.Types.status200 $ RawHtml [String.Interpolate.i|<!DOCTYPE html>
+<meta charset="utf-8">
+<style>
+
+.bars rect {
+  fill: steelblue;
+}
+
+.axis text {
+  font: 10px sans-serif;
+}
+
+.axis path, .axis line {
+  fill: none;
+  stroke: #000;
+  shape-rendering: crispEdges;
+}
+
+</style>
+<body>
+<script src="https://d3js.org/d3.v3.min.js"></script>
+<script>
+
+var userAges = #{userAgesJS};
+d3.select("body")
+  .datum(userAges)
+  .call(histogramChart());
+
+function histogramChart() {
+  var margin = {top: 0, right: 0, bottom: 20, left: 0},
+      width = 960,
+      height = 500;
+
+  var histogram = d3.layout.histogram(),
+      x = d3.scale.ordinal(),
+      y = d3.scale.linear(),
+      xAxis = d3.svg.axis().scale(x).orient("bottom").tickSize(6, 0);
+
+  function chart(selection) {
+    selection.each(function(data) {
+
+      // Compute the histogram.
+      data = histogram(data);
+
+      // Update the x-scale.
+      x   .domain(data.map(function(d) { return d.x; }))
+          .rangeRoundBands([0, width - margin.left - margin.right], .1);
+
+      // Update the y-scale.
+      y   .domain([0, d3.max(data, function(d) { return d.y; })])
+          .range([height - margin.top - margin.bottom, 0]);
+
+      // Select the svg element, if it exists.
+      var svg = d3.select(this).selectAll("svg").data([data]);
+
+      // Otherwise, create the skeletal chart.
+      var gEnter = svg.enter().append("svg").append("g");
+      gEnter.append("g").attr("class", "bars");
+      gEnter.append("g").attr("class", "x axis");
+
+      // Update the outer dimensions.
+      svg .attr("width", width)
+          .attr("height", height);
+
+      // Update the inner dimensions.
+      var g = svg.select("g")
+          .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+      // Update the bars.
+      var bar = svg.select(".bars").selectAll(".bar").data(data);
+      bar.enter().append("rect");
+      bar.exit().remove();
+      bar .attr("width", x.rangeBand())
+          .attr("x", function(d) { return x(d.x); })
+          .attr("y", function(d) { return y(d.y); })
+          .attr("height", function(d) { return y.range()[0] - y(d.y); })
+          .order();
+
+      // Update the x-axis.
+      g.select(".x.axis")
+          .attr("transform", "translate(0," + y.range()[0] + ")")
+          .call(xAxis);
+    });
+  }
+
+  chart.margin = function(_) {
+    if (!arguments.length) return margin;
+    margin = _;
+    return chart;
+  };
+
+  chart.width = function(_) {
+    if (!arguments.length) return width;
+    width = _;
+    return chart;
+  };
+
+  chart.height = function(_) {
+    if (!arguments.length) return height;
+    height = _;
+    return chart;
+  };
+
+  // Expose the histogram's value, range and bins method.
+  d3.rebind(chart, histogram, "value", "range", "bins");
+
+  // Expose the x-axis' tickFormat method.
+  d3.rebind(chart, xAxis, "tickFormat");
+
+  return chart;
+}
+
+</script>
+  |]
 
 postCreateUserR :: Yesod.HandlerFor FantasticSpoon Aeson.Value
 postCreateUserR = do
